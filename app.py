0080afe6bd843b6f01a7650164527e6e4c6443d5
Klaudiusz25 <klaudiuszkazmierowski@gmail.com>
2025-06-24 00:35:03 +0200

    Interfejs webowy z wykresem

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, render_template, request
 from weather import get_weather
 from storage import save_to_csv
+from plot import save_plot_to_file
 
 app = Flask(__name__)
 
@@ -8,16 +10,22 @@ app = Flask(__name__)
 def index():
     weather = None
     error = None
+    plot_path = None
 
     if request.method == 'POST':
         city = request.form.get('city')
         try:
             weather = get_weather(city)
             save_to_csv(weather)
+
+            # Ścieżka do pliku wykresu
+            filename = f'static/plot.png'
+            if save_plot_to_file(city, filename):
+                plot_path = filename
         except ValueError as e:
             error = str(e)
 
-    return render_template('index.html', weather=weather, error=error)
+    return render_template('index.html', weather=weather, error=error, plot_path=plot_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- plot.py
+++ plot.py
@@ -31,3 +31,37 @@ def plot_temperature_history(city):
     plt.xticks(rotation = 45)
     plt.tight_layout()
     plt.show()
+
+def save_plot_to_file(city, filename):
+
+    timestamps = []
+    temperatures = []
+
+    try:
+        with open("data/history.csv", encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                row = {k.lower(): v for k, v in row.items()}
+                if row['miasto'].lower() == city.lower():
+                    dt = datetime.strptime(row['data'], "%Y-%m-%d %H:%M")
+                    temp = float(row['temperatura'])
+                    timestamps.append(dt)
+                    temperatures.append(temp)
+    except FileNotFoundError:
+        return False
+
+    if not timestamps:
+        return False
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(timestamps, temperatures, marker='o', linestyle='-', color='dodgerblue')
+    plt.title(f"Temperatura w {city.capitalize()} (historia)")
+    plt.xlabel("Data")
+    plt.ylabel("Temperatura (°C)")
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+    return True
